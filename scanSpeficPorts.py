891ebceb7aae8ef844ebdb92974dd405295325c8
Krishpkreame <79666419+Krishpkreame@users.noreply.github.com>
2022-04-04 14:31:51 +1200

    Update scanSpeficPorts.py

--- scanSpeficPorts.py
+++ scanSpeficPorts.py
@@ -5,7 +5,7 @@ x = int(input("Starting port"))
 y = int(input("End port"))
 try:
     for i in range(x, y+1):
-        print("Trying port "+str(x))
+        print("Trying port "+str(i))
         os.system("wget -T 4 -t 1 5 -qO- 35.180.139.74:"+str(i))
 except:
     print("stopped")
